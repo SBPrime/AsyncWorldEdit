1b392df9d063d9dd241a35bd708b60bb42d1c39b
SBPrime <SB_Prime@poczta.onet.pl>
2015-05-01 22:20:16 +0200

    AWE CHG: Extracted progress display to seperate plugins

---
+++ progressDisplay/IProgressDisplay.java
@@ -0,0 +1,78 @@
+/*
+ * AsyncWorldEdit a performance improvement plugin for Minecraft WorldEdit plugin.
+ * Copyright (c) 2015, SBPrime <https://github.com/SBPrime/>
+ * Copyright (c) AsyncWorldEdit contributors
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted free of charge provided that the following 
+ * conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer. 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution,
+ * 3. Redistributions of source code, with or without modification, in any form 
+ *    other then free of charge is not allowed,
+ * 4. Redistributions in binary form in any form other then free of charge is 
+ *    not allowed.
+ * 5. Any derived work based on or containing parts of this software must reproduce 
+ *    the above copyright notice, this list of conditions and the following 
+ *    disclaimer in the documentation and/or other materials provided with the 
+ *    derived work.
+ * 6. The original author of the software is allowed to change the license 
+ *    terms or the entire license of the software as he sees fit.
+ * 7. The original author of the software is allowed to sublicense the software 
+ *    or its parts using any license terms he sees fit.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.primesoft.asyncworldedit.api.progressDisplay;
+
+import org.primesoft.asyncworldedit.playerManager.PlayerEntry;
+
+/**
+ *
+ * @author SBPrime
+ */
+public interface IProgressDisplay {
+    /**
+     * Get the progress display name
+     * @return 
+     */
+    String getName();
+
+    /**
+     * Disable (hide) the player message
+     * @param player 
+     */
+    void disableMessage(PlayerEntry player);
+
+    
+    /**
+     * Show player message
+     * @param player
+     * @param jobsCount Number of queued jobs
+     * @param queuedBlocks Current number of queued blocks
+     * @param maxQueuedBlocks Current maximum number of queued blocks
+     * @param timeLeft
+     * @param placingSpeed Current block placing sped (blocks per second)
+     * @param percentage Blocks placing done percentage
+     */
+    void setMessage(PlayerEntry player, 
+            int jobsCount, 
+            int queuedBlocks, int maxQueuedBlocks,
+            double timeLeft, double placingSpeed, double percentage);
+    
+}
