3d96aaf6b63162cc708376bb28a4258bbacfb738
SBPrime <SB_Prime@poczta.onet.pl>
2018-03-04 16:54:02 +0100

    Migrated API to maven

--- AsyncWorldEdit-API/src/main/java/org/primesoft/asyncworldedit/api/io/SeekOrigin.java
+++ AsyncWorldEdit-API/src/main/java/org/primesoft/asyncworldedit/api/io/SeekOrigin.java
@@ -45,7 +45,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package org.primesoft.asyncworldedit.utils.io;
+package org.primesoft.asyncworldedit.api.io;
 
 /**
  *
